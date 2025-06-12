5b80e91278b9ce676b192dabe2468a61c0d20a0c
nabinr221 <nabinr221@users.noreply.github.com>
2025-06-12 08:10:28 +0000

    deploy: da08a32a55abfc6397c3b897233d0b8460ac89c4

---
+++ assets/SocialMediaIcon-_TDy0BeQ.js
@@ -0,0 +1,16 @@
+import{c as a,j as e,L as t,G as i}from"./index-DlHFLAg6.js";/**
+ * @license lucide-react v0.372.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=a("Linkedin",[["path",{d:"M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z",key:"c2jq9f"}],["rect",{width:"4",height:"12",x:"2",y:"9",key:"mk3on5"}],["circle",{cx:"4",cy:"4",r:"2",key:"bt5ra8"}]]);/**
+ * @license lucide-react v0.372.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=a("Mail",[["rect",{width:"20",height:"16",x:"2",y:"4",rx:"2",key:"18n3k1"}],["path",{d:"m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7",key:"1ocrg3"}]]);/**
+ * @license lucide-react v0.372.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=a("Smartphone",[["rect",{width:"14",height:"20",x:"5",y:"2",rx:"2",ry:"2",key:"1yt0o3"}],["path",{d:"M12 18h.01",key:"mhygvu"}]]),l=()=>e.jsxs("div",{className:"flex gap-2 me-7",children:[e.jsx(t,{to:"https://github.com/nabinr221",target:"_blank",children:e.jsx("div",{className:"w-8 h-8 p-1.5 flex justify-center items-center bg-black rounded-full border hover:bg-[#656464] transition-all duration-300",children:e.jsx(i,{className:"text-white"})})}),e.jsx(t,{to:"https://www.linkedin.com/in/nabinr221/",target:"_blank",children:e.jsx("div",{className:"w-8 h-8 p-1.5 flex justify-center items-center bg-black rounded-full border hover:bg-[#656464] transition-all duration-300",children:e.jsx(r,{className:"text-white"})})}),e.jsx(t,{to:"mailto:nabinrchy@gmail.com",target:"_blank",children:e.jsx("div",{className:"w-8 h-8 p-1.5 flex justify-center items-center bg-black rounded-full border hover:bg-[#656464] transition-all duration-300",children:e.jsx(n,{className:"text-white"})})})]});export{n as M,l as S,c as a};
